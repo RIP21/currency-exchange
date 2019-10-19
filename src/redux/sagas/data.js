10a132c7edf8c0212f9280a46a2f9f24c55b1741
Andrii Los <andrii.los@revolut.com>
2019-10-20 00:38:16 +0200

    Mock data

--- src/redux/sagas/data.js
+++ src/redux/sagas/data.js
@@ -1,16 +1,41 @@
 import { all, put, fork, call } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
 import { LOAD_RATES } from '../ducks/data';
-import axios from 'axios';
 import keyBy from 'lodash/keyBy';
+
+const fakeRequest = data => {
+  return new Promise(resolve => {
+    resolve({
+      data
+    });
+  });
+};
 
 function* fetchRates() {
   yield put({ type: LOAD_RATES.REQUEST });
   try {
     const responses = yield all([
-      call(axios.get, 'https://api.fixer.io/latest?base=EUR&symbols=USD,GBP'),
-      call(axios.get, 'https://api.fixer.io/latest?base=USD&symbols=EUR,GBP'),
-      call(axios.get, 'https://api.fixer.io/latest?base=GBP&symbols=USD,EUR')
+      call(fakeRequest, {
+        base: 'EUR',
+        rates: {
+          USD: 1.1165,
+          GBP: 0.859838
+        }
+      }),
+      call(fakeRequest, {
+        base: 'USD',
+        rates: {
+          EUR: 0.8956,
+          GBP: 0.7
+        }
+      }),
+      call(fakeRequest, {
+        base: 'GBP',
+        rates: {
+          EUR: 1.25,
+          USD: 1.4
+        }
+      })
     ]);
     const data = keyBy(responses.map(response => response.data), 'base');
     yield put({ type: LOAD_RATES.SUCCESS, payload: { data } });
